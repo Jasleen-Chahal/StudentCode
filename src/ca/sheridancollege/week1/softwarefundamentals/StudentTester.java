853a9f07869e0b9b1071a13a86346a3739a3a5bc
Jasleen-Chahal <chahjasl@sheridancollege.ca>
2019-05-15 10:58:48 -0400

    Initial commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
@@ -9,25 +9,26 @@ package ca.sheridancollege.week1.softwarefundamentals;
  *
  * @author mehta
  */
-public class StudentTester {
-    public static void main(String[] args)
-    {
-        //Array of Objects
-        Student[] studentList=new Student[3];
-        
-        //Object of Student class
-        Student s1=new Student("Harpreet",347);
-        
-        //Populating array with Object
-        studentList[0]=s1;
-        
-        studentList[1]=new Student("Leo",123);
-        studentList[2]=new Student("Sheetal",111);
-        
-        for(int i=0;i<studentList.length;i++)
-        {
-            System.out.println(studentList[i].getName());
-        }
-        
-    }
+public class StudentTester
+{
+   public static void main (String[] args)
+   {
+      //Array of Objects
+      Student[] studentList = new Student[3];
+
+      //Object of Student class
+      Student s1 = new Student("Harpreet", 347);
+
+      //Populating array with Object
+      studentList[0] = s1;
+
+      studentList[1] = new Student("Leo", 123);
+      studentList[2] = new Student("Sheetal", 111);
+
+      for (int i = 0; i < studentList.length; i++) {
+         System.out.println(studentList[i].getName());
+         System.out.println("Welcome");
+      }
+
+   }
 }
